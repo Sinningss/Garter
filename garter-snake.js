d1866c3aa59b3a7d23345ca648c01a592835d582
LukasExists <91223726+lukasexists@users.noreply.github.com>
2021-11-19 00:40:22 -0500

    Update garter-snake.js

--- garter-snake.js
+++ garter-snake.js
@@ -9,20 +9,19 @@ snake.garter = function(settings = {}) {
   
   // STAGE SIZES //
   
+  img = new Image; // Custom Stage
+  img.src = 'https://raw.githubusercontent.com/lukasexists/GoogleSnakeModAttempt/main/img/custom.png';
+  img.width = 47;
+  img.height = 47;
+  document.querySelector('#size').appendChild(img);
+  
   img = new Image; // Tiny Stage
   img.src = 'https://raw.githubusercontent.com/lukasexists/GoogleSnakeModAttempt/main/img/tiny.png';
   img.width = 47;
   img.height = 47;
   document.querySelector('#size').appendChild(img);
   
-  // THEMES //
-  
-  img = new Image; // Dark Theme (not working)
-  img.src = 'https://raw.githubusercontent.com/lukasexists/GoogleSnakeModAttempt/main/img/darktheme.png';
-  img.width = 47;
-  img.height = 47;
-  document.querySelector('#color').appendChild(img);
-  
+  // actual stuff
   const scripts = document.getElementsByTagName('script');
   for(let script of scripts) {
     const req = new XMLHttpRequest();
@@ -38,7 +37,7 @@ snake.garter = function(settings = {}) {
           /[a-zA-Z0-9_$]{1,6}=function\(a\){switch\(a\.[a-zA-Z0-9_$]{1,6}\){case 2:return 512;[^]*?256}}/
         )[0].replace(
           '96;',
-          `96;case 3:return 36;case 4:return 36;case 5:return 1200;case 6:return 3500;case 7:return 10000;case 8:return 25000;case 9:return 100000;`
+          `96;case 3:return 36;case 4:return 1;case 5:return 1200;case 6:return 3500;case 7:return 10000;case 8:return 25000;case 9:return 100000;`
         )
       );
     };
