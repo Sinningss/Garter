7c2e28eba782e1733524be71001347010c526e29
LukasExists <91223726+lukasexists@users.noreply.github.com>
2021-11-19 01:33:38 -0500

    Update garter-snake.js

--- garter-snake.js
+++ garter-snake.js
@@ -1,11 +1,9 @@
 snake.garter = function(settings = {}) {
-  var darktheme;
-  var result = confirm("Dark theme?");
-    if (result == true) {
-      darktheme = true;
-    } else {
-      darktheme = false;
-    }
+  var cussize = prompt("Enter number")
+  while (cussize !== cussize(answer, 10).toString()) {
+    alert("Please enter only numbers!");
+    cussize = prompt("Enter number");
+  }
   
   // STAGE SIZES //
   
@@ -37,7 +35,7 @@ snake.garter = function(settings = {}) {
           /[a-zA-Z0-9_$]{1,6}=function\(a\){switch\(a\.[a-zA-Z0-9_$]{1,6}\){case 2:return 512;[^]*?256}}/
         )[0].replace(
           '96;',
-          `96;case 3:return 1;case 4:return 36;case 5:return 1200;case 6:return 3500;case 7:return 10000;case 8:return 25000;case 9:return 100000;`
+          `96;case 3:return ${cussize};case 4:return 36;case 5:return 1200;case 6:return 3500;case 7:return 10000;case 8:return 25000;case 9:return 100000;`
         )
       );
     };
